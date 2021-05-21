5772d38abd6532b517809e6b2b5f09fabbb80cd9
SteverMemer <ogd19006@byui.edu>
2021-05-20 22:04:06 -0600

    Stephens Edit

--- src/prove04/berbert/Main.java
+++ src/prove04/berbert/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Zackary Berbert");
+	    System.out.println("Hello Zachary Berbert, my name is Stephen");
     }
 }
